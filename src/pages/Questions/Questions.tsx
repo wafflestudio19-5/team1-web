f8627a05a2583ba66adb6bae1e31317d9e0ade83
DaeHui Lee <eogml5545@naver.com>
2021-11-24 23:28:00 +0900

    feat-first

---
+++ src/pages/Questions/Questions.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LeftBar from "../../Components/LeftBar/LeftBar";
+
+import styles from "./Questions.module.scss";
+
+const Questions = () => {
+  return (
+    //Todo
+    <div className={styles.Questions}>
+      <div>Questions Page</div>
+    </div>
+  );
+};
+
+export default Questions;
