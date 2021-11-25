13b9f9b5147c1bc642d12a5f87f0c7e87110e556
kmsgm <kmsgm@snu.ac.kr>
2021-11-26 01:08:25 +0900

    Add: Header & Ask Button

--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
+import ButtonUnstyled from '@mui/base/ButtonUnstyled';
+
+import styles from './Question.module.scss';
+
 const Question: React.FC = () => {
-  return <div>Question Page hello</div>;
+  return (
+    <div className={styles.Question}>
+      <div className={styles.Content}>
+        <div className={styles.innerContent}>
+          <div className={styles.questionHeader}>
+            <h1>
+              Cannot chagne the dorm in the elements panel while source is
+              paused in Chrome 96 dev tools - do I miss a setting?
+            </h1>
+            <ButtonUnstyled className={styles.askButton}>
+              <p>Ask Question</p>
+            </ButtonUnstyled>
+          </div>
+          <ul className={styles.postInfo}>
+            <li>
+              <span>Asked</span>
+              <time>today</time>
+            </li>
+            <li>
+              <span>Active</span>
+              <time>today</time>
+            </li>
+            <li>
+              <span>Viewd</span>9 times
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Question;
