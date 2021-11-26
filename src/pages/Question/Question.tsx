ecbc1e62a17f53800897d58c7d639389edbf8c5d
kmsgm <kmsgm@snu.ac.kr>
2021-11-27 04:30:48 +0900

    Add: post Answer

--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import ButtonUnstyled from '@mui/base/ButtonUnstyled';
+import { Link } from 'react-router-dom';
 
 import { ReactComponent as ArrowDown } from '../../icons/iconArrowDown.svg';
 import { ReactComponent as ArrowUp } from '../../icons/iconArrowUp.svg';
@@ -10,89 +11,103 @@ import styles from './Question.module.scss';
 
 const Question: React.FC = () => {
   const [vote, setVote] = useState<number>(0);
+  const [answers, setAnswers] = useState<Array<string>>();
+  const [answer, setAnswer] = useState<string>('');
+
   const onIncrease = () => setVote((prev) => prev + 1);
   const onDecrease = () => setVote((prev) => prev - 1);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAnswer(e.target.value);
+  };
 
   return (
     <div className={styles.Question}>
       <div className={styles.Content}>
-        <div className={styles.innerContent}>
-          <section className={styles.questionHeader}>
-            <h1>
-              Cannot chagne the dorm in the elements panel while source is
-              paused in Chrome 96 dev tools - do I miss a setting?
-            </h1>
-            <ButtonUnstyled className={styles.askButton}>
-              <p>Ask Question</p>
-            </ButtonUnstyled>
-          </section>
-          <ul className={styles.postInfo}>
-            <li>
-              <span>Asked</span>
-              <time>today</time>
-            </li>
-            <li>
-              <span>Active</span>
-              <time>today</time>
-            </li>
-            <li>
-              <span>Viewd</span>9 times
-            </li>
-          </ul>
+        <section className={styles.questionHeader}>
+          <h1>
+            Cannot chagne the dorm in the elements panel while source is paused
+            in Chrome 96 dev tools - do I miss a setting?
+          </h1>
+          <ButtonUnstyled className={styles.askButton}>
+            <Link to="/question/ask">Ask Question</Link>
+          </ButtonUnstyled>
+        </section>
+        <ul className={styles.postInfo}>
+          <li>
+            <span>Asked</span>
+            <time>today</time>
+          </li>
+          <li>
+            <span>Active</span>
+            <time>today</time>
+          </li>
+          <li>
+            <span>Viewd</span>9 times
+          </li>
+        </ul>
 
-          <section className={styles.main}>
-            <div className={styles.questionSection}>
-              <div className={styles.postLayout}>
-                <div className={`${styles.voteCell} ${styles.postLayoutLeft}`}>
-                  <div className={styles.voteBox}>
-                    <button className="vote-up" onClick={onIncrease}>
-                      <ArrowUp />
-                    </button>
-                    <div className={styles.voteCount}>{vote}</div>
-                    <button className="vote-down" onClick={onDecrease}>
-                      <ArrowDown />
-                    </button>
-                    <button className="bookmark">
-                      <Bookmark />
-                    </button>
-                  </div>
-                </div>
-                <div className={`${styles.postCell} ${styles.postLayoutRight}`}>
-                  <div className={styles.postBody}>
-                    <p>
-                      Is there any way to revert or undo git pull so that my
-                      source/repos will come to old state that was before doing
-                      git pull ? I want to do this because it merged some files
-                      which I didnt want to do so, but only merge other
-                      remaining files. So, I want to get those files back, is
-                      that possible?
-                    </p>
-                  </div>
-                  <div className={styles.Tag}>
-                    <ul className={styles.tagList}>
-                      <li>git</li>
-                      <li>version-control</li>
-                      <li>git-merge</li>
-                    </ul>
-                  </div>
-                  <div className={styles.footer}></div>
-                </div>
-                <div className={styles.postComment}>
-                  <div className={styles.Comments}>
-                    <ul className={styles.commentList}>
-                      <li>Sounds like a bug in devtools.</li>
-                      <li>
-                        yeah. it looks like its buggy on local websites, not on
-                        live ones
-                      </li>
-                    </ul>
-                  </div>
-                  <div className={styles.CommentCell}>Add a comment</div>
-                </div>
+        <section className={styles.main}>
+          <div className={styles.postLayout}>
+            <div className={styles.voteCell}>
+              <button className="vote-up" onClick={onIncrease}>
+                <ArrowUp />
+              </button>
+              <div className={styles.voteCount}>{vote}</div>
+              <button className="vote-down" onClick={onDecrease}>
+                <ArrowDown />
+              </button>
+              <button className="bookmark">
+                <Bookmark />
+              </button>
+            </div>
+            <div className={styles.postCell}>
+              <div className={styles.postBody}>
+                <p>
+                  Is there any way to revert or undo git pull so that my
+                  source/repos will come to old state that was before doing git
+                  pull ? I want to do this because it merged some files which I
+                  didnt want to do so, but only merge other remaining files. So,
+                  I want to get those files back, is that possible?
+                </p>
+              </div>
+              <ul className={styles.tagList}>
+                <li>git</li>
+                <li>version-control</li>
+                <li>git-merge</li>
+              </ul>
+
+              <div className={styles.Footer}>
+                <ul className={styles.postMenuList}>
+                  <button>Share</button>
+                  <button>Edit</button>
+                  <button>Follow</button>
+                </ul>
+                <div className={styles.postSignature}>User Info</div>
               </div>
             </div>
-          </section>
-        </div>
+          </div>
+          <div className={styles.Answer}>
+            {answers !== undefined && answer.length > 0 ? (
+              <>
+                {answers.map((answer, id) => (
+                  <div key={id}>{answer}</div>
+                ))}
+              </>
+            ) : null}
+            <form>
+              <h2>Your Answer</h2>
+              <input
+                className={styles.postAnswer}
+                value={answer}
+                onChange={onChange}
+              />
+              <ButtonUnstyled className={styles.postButton} type="submit">
+                Post Your Answer
+              </ButtonUnstyled>
+            </form>
+          </div>
+        </section>
       </div>
     </div>
   );
