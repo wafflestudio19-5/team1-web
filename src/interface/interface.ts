1feacfaafa8b5e08cdd23c2642cbe7a908dde7db
kmsgm <kmsgm@snu.ac.kr>
2021-12-01 22:33:21 +0900

    Add: interface

---
+++ src/interface/interface.ts
@@ -0,0 +1,48 @@
+export interface User {
+    id: number;
+    name: string;
+    profile: string;
+}
+
+export interface Tag {
+    id: number;
+    name: string;
+    intro: string;
+}
+
+export interface Vote {
+    id: number;
+    user: User;
+    status: string;
+    questionId: number;
+    answerId: number;
+}
+
+export interface Comment {
+    id: number;
+    user: User;
+    body: string;
+    questionId: number;
+    answerId: number;
+}
+
+export interface Answer {
+    id: number;
+    user: User;
+    body: string;
+    votes: Vote[];
+    comments: Comment[];
+    accpedted: boolean;
+}
+
+export interface QuestionInterface {
+    id: number;
+    user: User;
+    title: string;
+    body: string;
+    votes: Vote[];
+    comments: Comment[];
+    tags: Tag[];
+    answers: Answer[];
+    createdAt: string;
+}
