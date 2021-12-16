c676c0d25e1100f870db273814cd79f5220a942f
joongwon <winterdawnlight@snu.ac.kr>
2021-12-17 00:10:05 +0900

    make api functions async

--- src/api/api.ts
+++ src/api/api.ts
@@ -29,72 +29,100 @@ export const setAccessToken = (token: string | null) => {
   storeToken(token);
 };
 export const getAccessToken = () => loadToken();
+setHeaderToken(loadToken());
 
 export interface EmptyBody {}
 
 export const ping = () => instance.get("/api/v1/pingpong/");
 export const api = {
   // TODO: signin api must return jwt token
-  signin: (email: string, password: string) =>
-    instance.post<EmptyBody>("/api/user/signin/", {
-      email: email,
-      password: password,
-    }),
-  signup: (name: string, email: string, password: string) =>
-    instance.post<EmptyBody>("/api/user/signup/", {
-      name: name,
-      email: email,
-      password: password,
-    }),
-  getQuestionList: () => instance.get<QuestionInterface[]>("/api/question/"),
-  postQuestion: (title: string, body: string) =>
-    instance.post<QuestionInterface>("/api/question/", {
-      title: title,
-      body: body,
-    }),
-  getQuestion: (id: number) =>
-    instance.get<QuestionInterface>(`/api/question/${id}/`),
-  editQuestion: (id: number, title: string, body: string) =>
-    instance.put<QuestionInterface>(`/api/question/${id}/`, {
-      title: title,
-      body: body,
-    }),
-  deleteQuestion: (id: number) =>
-    instance.delete<EmptyBody>(`/api/question/${id}/`),
-  getQuestionCommentList: (questionId: number) =>
-    instance.get<Comment[]>(`/api/question/${questionId}/comment/`),
-  postQuestionComment: (questionId: number, body: string) =>
-    instance.post<Comment>(`/api/question/${questionId}/comment/`, {
-      body: body,
-    }),
-  editQuestionComment: (questionId: number, commentId: number, body: string) =>
-    instance.put<Comment>(`/api/question/${questionId}/comment/${commentId}/`, {
-      body: body,
-    }),
-  deleteQuestionComment: (questionId: number, commentId: number) =>
-    instance.delete<EmptyBody>(
-      `/api/question/${questionId}/comment/${commentId}/`
-    ),
-  postAnswer: (questionId: number, title: string, body: string) =>
-    instance.post<Answer>(`/api/question/${questionId}/answer/`, {
-      title: title,
-      body: body,
-    }),
-  editAnswer: (answerId: number, title: string, body: string) =>
-    instance.put<Answer>(`/api/answer/${answerId}`, {
-      title: title,
-      body: body,
-    }),
-  deleteAnswer: (answerId: number) =>
-    instance.delete<EmptyBody>(`/api/answer/${answerId}`),
+  signin: async (email: string, password: string) =>
+    (
+      await instance.post<EmptyBody>("/api/user/signin/", {
+        email: email,
+        password: password,
+      })
+    ).data,
+  signup: async (name: string, email: string, password: string) =>
+    (
+      await instance.post<EmptyBody>("/api/user/signup/", {
+        name: name,
+        email: email,
+        password: password,
+      })
+    ).data,
+  getQuestionList: async () =>
+    (await instance.get<QuestionInterface[]>("/api/question/")).data,
+  postQuestion: async (title: string, body: string) =>
+    (
+      await instance.post<QuestionInterface>("/api/question/", {
+        title: title,
+        body: body,
+      })
+    ).data,
+  getQuestion: async (id: number) =>
+    (await instance.get<QuestionInterface>(`/api/question/${id}/`)).data,
+  editQuestion: async (id: number, title: string, body: string) =>
+    (
+      await instance.put<QuestionInterface>(`/api/question/${id}/`, {
+        title: title,
+        body: body,
+      })
+    ).data,
+  deleteQuestion: async (id: number) =>
+    (await instance.delete<EmptyBody>(`/api/question/${id}/`)).data,
+  getQuestionCommentList: async (questionId: number) =>
+    (await instance.get<Comment[]>(`/api/question/${questionId}/comment/`))
+      .data,
+  postQuestionComment: async (questionId: number, body: string) =>
+    (
+      await instance.post<Comment>(`/api/question/${questionId}/comment/`, {
+        body: body,
+      })
+    ).data,
+  editQuestionComment: async (
+    questionId: number,
+    commentId: number,
+    body: string
+  ) =>
+    (
+      await instance.put<Comment>(
+        `/api/question/${questionId}/comment/${commentId}/`,
+        {
+          body: body,
+        }
+      )
+    ).data,
+  deleteQuestionComment: async (questionId: number, commentId: number) =>
+    (
+      await instance.delete<EmptyBody>(
+        `/api/question/${questionId}/comment/${commentId}/`
+      )
+    ).data,
+  postAnswer: async (questionId: number, title: string, body: string) =>
+    (
+      await instance.post<Answer>(`/api/question/${questionId}/answer/`, {
+        title: title,
+        body: body,
+      })
+    ).data,
+  editAnswer: async (answerId: number, title: string, body: string) =>
+    (
+      await instance.put<Answer>(`/api/answer/${answerId}`, {
+        title: title,
+        body: body,
+      })
+    ).data,
+  deleteAnswer: async (answerId: number) =>
+    (await instance.delete<EmptyBody>(`/api/answer/${answerId}`)).data,
 
   // TODO: need up/down parameter
-  voteUpQuestion: (questionId: number) =>
-    instance.post<EmptyBody>(`/api/question/${questionId}/vote/`),
-  voteDownQuestion: (questionId: number) =>
-    instance.post<EmptyBody>(`/api/question/${questionId}/vote/`),
-  voteUpAnswer: (answerId: number) =>
-    instance.post(`/api/answer/${answerId}/vote/`),
-  voteDownAnswer: (answerId: number) =>
-    instance.post(`/api/answer/${answerId}/vote/`),
+  voteUpQuestion: async (questionId: number) =>
+    (await instance.post<EmptyBody>(`/api/question/${questionId}/vote/`)).data,
+  voteDownQuestion: async (questionId: number) =>
+    (await instance.post<EmptyBody>(`/api/question/${questionId}/vote/`)).data,
+  voteUpAnswer: async (answerId: number) =>
+    (await instance.post(`/api/answer/${answerId}/vote/`)).data,
+  voteDownAnswer: async (answerId: number) =>
+    (await instance.post(`/api/answer/${answerId}/vote/`)).data,
 };
