50714edba70d03ceda4a8b020536431e645f7c3b
kmsgm <kmsgm@snu.ac.kr>
2021-12-18 21:37:10 +0900

    Fix: Add & Edit page code

--- src/pages/Ask/Ask.tsx
+++ src/pages/Ask/Ask.tsx
@@ -14,49 +14,47 @@ const Ask: React.FC = () => {
   };
 
   return (
-    <>
-      <div className={styles.ask}>
-        <div className={styles.header}>
-          <h1>Ask a public question</h1>
-        </div>
+    <div className={styles.ask}>
+      <div className={styles.header}>
+        <h1>Ask a public question</h1>
+      </div>
 
-        <div className={styles.content}>
-          <div className={styles.title}>
-            <label>Title</label>
-            <p className={styles.tip}>
-              Be specific and imagine you’re asking a question to another person
-            </p>
-            <input
-              value={title}
-              maxLength={300}
-              placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
-              onChange={handleTitleChange}
-            />
-          </div>
-          <div className={styles.body}>
-            <label>Body</label>
-            <p className={styles.tip}>
-              Include all the information someone would need to answer your
-              question
-            </p>
-            <Markdown state={body} setState={setBody} />
-          </div>
-          <div className={styles.tags}>
-            <label>Tags</label>
-            <p className={styles.tip}>
-              Add up to 5 tags to describe what your question is about
-            </p>
-            <input
-              maxLength={300}
-              placeholder="e.g. (asp.net-mvc typescript database)"
-            />
-          </div>
+      <div className={styles.content}>
+        <div className={styles.title}>
+          <label>Title</label>
+          <p className={styles.tip}>
+            Be specific and imagine you’re asking a question to another person
+          </p>
+          <input
+            value={title}
+            maxLength={300}
+            placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
+            onChange={handleTitleChange}
+          />
         </div>
-        <div className={styles.postButton}>
-          <BlueButton text={"Post your question"} />
+        <div className={styles.body}>
+          <label>Body</label>
+          <p className={styles.tip}>
+            Include all the information someone would need to answer your
+            question
+          </p>
+          <Markdown state={body} setState={setBody} />
+        </div>
+        <div className={styles.tags}>
+          <label>Tags</label>
+          <p className={styles.tip}>
+            Add up to 5 tags to describe what your question is about
+          </p>
+          <input
+            maxLength={300}
+            placeholder="e.g. (asp.net-mvc typescript database)"
+          />
         </div>
       </div>
-    </>
+      <div className={styles.postButton}>
+        <BlueButton text={"Post your question"} />
+      </div>
+    </div>
   );
 };
 
--- src/pages/Edit/Edit.tsx
+++ src/pages/Edit/Edit.tsx
@@ -18,6 +18,7 @@ const Ask: React.FC<EditProps> = ({ title, body, isQuestion }) => {
 
   useEffect(() => {
     setEditedTitle(title ? title : "");
+    setEditedBody(body ? body : "");
   }, []);
 
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -25,37 +26,35 @@ const Ask: React.FC<EditProps> = ({ title, body, isQuestion }) => {
   };
 
   return (
-    <>
-      <div className={styles.ask}>
-        <div className={styles.content}>
-          <div className={styles.title}>
-            <label>Title</label>
-            <input
-              maxLength={300}
-              value={editedTitle}
-              onChange={handleTitleChange}
-            />
-          </div>
-          <div className={styles.body}>
-            <label>Body</label>
-            <Markdown state={editedBody} setState={setEditedBody} />
-          </div>
-          <div className={styles.tags}>
-            <label>Tags</label>
-            <p className={styles.tip}>
-              Add up to 5 tags to describe what your question is about
-            </p>
-            <input
-              maxLength={300}
-              placeholder="e.g. (asp.net-mvc typescript database)"
-            />
-          </div>
+    <div className={styles.ask}>
+      <div className={styles.content}>
+        <div className={styles.title}>
+          <label>Title</label>
+          <input
+            maxLength={300}
+            value={editedTitle}
+            onChange={handleTitleChange}
+          />
         </div>
-        <div className={styles.postButton}>
-          <BlueButton text={"Post your question"} />
+        <div className={styles.body}>
+          <label>Body</label>
+          <Markdown state={editedBody} setState={setEditedBody} />
+        </div>
+        <div className={styles.tags}>
+          <label>Tags</label>
+          <p className={styles.tip}>
+            Add up to 5 tags to describe what your question is about
+          </p>
+          <input
+            maxLength={300}
+            placeholder="e.g. (asp.net-mvc typescript database)"
+          />
         </div>
       </div>
-    </>
+      <div className={styles.postButton}>
+        <BlueButton text={"Post your question"} />
+      </div>
+    </div>
   );
 };
 
