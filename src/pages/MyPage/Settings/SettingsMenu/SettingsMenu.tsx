ff6925eedaa4592674fd6a659696afae198823dd
joongwon <winterdawnlight@snu.ac.kr>
2021-12-21 20:19:37 +0900

    Add: menu styles

--- src/pages/MyPage/Settings/SettingsMenu/SettingsMenu.tsx
+++ src/pages/MyPage/Settings/SettingsMenu/SettingsMenu.tsx
@@ -4,11 +4,18 @@ import styles from "./SettingsMenu.module.scss";
 
 interface SettingsMenuProps {}
 
+enum SETTINGS_MENU_ITEM {}
+
 export const SettingsMenu: FC<SettingsMenuProps> = ({}) => {
+  const selectedMenu = SETTINGS_MENU_ITEM;
   return (
     <ul className={styles.SettingsMenu}>
       <li className={styles.header}>PERSONAL INFORMATION</li>
-      <li className={styles.item}>
+      <li
+        className={`${styles.item} ${
+          selectedMenu === SETTINGS_MENU_ITEM ? styles.selected : ""
+        }`}
+      >
         {/* TODO: change url */}
         <Link to={`/mypage?tab=settings`}>Edit profile</Link>
       </li>
