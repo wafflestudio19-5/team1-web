ee2be1b6113d26fdf7641a3e46414cbebe4bde3e
Joongwon Ahn <winterdawnlight@seoul.ac.kr>
2022-01-08 00:03:40 +0900

    Fix: signin page redirect

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -9,7 +9,7 @@ import { useSessionContext } from "../../contexts/SessionContext";
 
 const Login = () => {
   const { userInfo } = useSessionContext();
-  return userInfo !== null ? (
+  return userInfo ? (
     <Navigate to={"/questions"} />
   ) : (
     <div className={styles.login}>
